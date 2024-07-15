4036a7dd3b7cfb965c55b3efca2ecdfee42a310b
Padma Dev <padma.dev@pixis.ai>
2024-07-15 15:34:03 +0530

    cleanup 1

---
+++ src/app/dev_test/page.jsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react';
+import {getClientCookie} from "@/app/utils/clientCookie";
+import {examTokenKey, refreshKey, tokenKey} from "@/app/utils/constants";
+
+export default function page() {
+    const token = getClientCookie(tokenKey)
+    const refresh = getClientCookie(refreshKey)
+    const examToken = getClientCookie(examTokenKey)
+
+    return (
+        <div>
+            <h1>aa</h1>
+            <h1> Token :{token} | end</h1>
+            <h1> Refresh :{refresh} | end</h1>
+            <h1> Exam Token :{examToken} | end</h1>
+        </div>
+    );
+}
